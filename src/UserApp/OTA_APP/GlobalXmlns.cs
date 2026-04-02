7020f4d4f50aa2b69344ceae541ead62a8813778
Ji-Wook Heo <jwheo92@gmail.com>
2026-04-03 02:53:27 +0900

    모두 커밋

---
+++ src/UserApp/OTA_APP/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "OTA_APP")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "OTA_APP.Pages")]
